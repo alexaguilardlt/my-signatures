0bdded6503defa8c694e0d80f9c0e8aa6317d236
alexaguilardlt <alexaguilardlt@gmail.com>
2024-11-22 14:02:20 +0100

    Add logout user and middleware auth

--- pages/login.vue
+++ pages/login.vue
@@ -1,43 +1,60 @@
-<template>
-  <div>
-    <button @click="signInWithOtp">Sign In with Email</button>
-    <input type="email" v-model="email" />
-  </div>
-  <div>
-    <button @click="probando">Boton de prueba</button>
-    <button @click="signUpNewUser">Crear cuenta nueva</button>
-    <input type="email" v-model="email" />
-    <input type="password" v-model="password" />
-  </div>
-</template>
-
 <script setup lang="ts">
 const supabase = useSupabaseClient()
 const email = ref('')
 const password = ref('')
-
-const signInWithOtp = async () => {
-  const { error } = await supabase.auth.signInWithOtp({
-    email: email.value,
-    options: {
-      shouldCreateUser: false,
-      emailRedirectTo: 'http://localhost:3000/confirm'
-    }
-  })
-  if (error) console.log(error)
-}
+const emailLogin = ref('')
+const passwordLogin = ref('')
 
 const signUpNewUser = async () => {
-  console.log('entro aqui')
   const { data, error } = await supabase.auth.signUp({
     email: email.value,
     password: password.value,
-    options: {
-      emailRedirectTo: 'http://localhost:3000/welcome'
-    }
   })
+
+  if (error) {
+    console.error("Error en el registro:", error.message)
+  } else {
+    navigateTo('/welcome')
+  }
 }
-const probando = (event) => {
-  console.log('probando', event)
+
+const loginUser  = async () => {
+  const { user, error } = await supabase.auth.signInWithPassword({
+    email: emailLogin.value,
+    password: passwordLogin.value,
+  })
+
+  if (error) {
+    if (error.message === 'Email not confirmed') {
+    } else {
+      console.error("Error en el login:", error.message)
+    }
+  } else {
+    navigateTo('/welcome')
+  }
 }
+
+async function resendConfirmationEmail(email) {
+  const { error } = await supabase.auth.api.resendConfirmationEmail(email)
+
+  if (error) {
+    console.error("Error al reenviar el correo de confirmación:", error.message)
+  } else {
+    console.log("Correo de confirmación reenviado con éxito")
+  }
+}
+
 </script>
+
+<template>
+  <div>
+    <button @click="signUpNewUser">Crear cuenta nueva</button>
+    <input type="email" v-model="email" />
+    <input type="password" v-model="password" />
+  </div>
+  <div>
+    <button @click="loginUser">Login</button>
+    <input type="email" v-model="emailLogin" />
+    <input type="password" v-model="passwordLogin" />
+  </div>
+</template>
--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+async function logoutUser() {
+  const { error } = await supabase.auth.signOut()
+
+  if (error) {
+    console.error("Error al cerrar sesión:", error.message)
+  } else {
+    console.log("Sesión cerrada con éxito")
+    // Redirigir al usuario a la página de inicio o de login
+    navigateTo('/') // O la URL que prefieras
+  }
+}
+
+definePageMeta({
+  middleware: 'auth'
+})
+</script>
 <template>
   <h1>Welcome page</h1>
+  <button @click="logoutUser">Logout</button>
 </template>
